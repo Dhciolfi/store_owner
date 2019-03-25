2a9cc6ca3a92e64eacfb60eac95ecbc4a0a0a655
Dhciolfi <daniel.ciolfi@gmail.com>
2019-03-24 23:01:18 -0300

    Changed product_screen to use scoped model

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,9 +1,8 @@
-import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:store_owner/widgets/image_source_sheet.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:store_owner/models/product_model.dart';
+import 'package:store_owner/widgets/images_widget.dart';
 
 class ProductScreen extends StatefulWidget {
 
@@ -21,265 +20,160 @@ class _ProductScreenState extends State<ProductScreen> {
   final _formKey = GlobalKey<FormState>();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
-  DocumentSnapshot product;
-  String categoryId;
+  final ProductModel _productModel;
 
-  Map<String, dynamic> unsavedData;
-  List<File> imagesToUpload = [];
-
-  _ProductScreenState(this.product, this.categoryId);
-
-  @override
-  void initState() {
-    super.initState();
-
-    if(product != null){
-      unsavedData = Map.of(product.data);
-      unsavedData["images"] = List<String>.from(product.data["images"]);
-      unsavedData["sizes"] = List<String>.from(product.data["sizes"]);
-    } else {
-      unsavedData = {
-        "images": [], "title": null, "description": null, "price": null, "sizes": []
-      };
-    }
-  }
-
-  Future uploadImages(String productId) async {
-    for(File img in imagesToUpload){
-      StorageUploadTask uploadTask = FirebaseStorage.instance.ref().child(categoryId).
-        child(productId).child(DateTime.now().millisecondsSinceEpoch.toString()).putFile(img);
-      StorageTaskSnapshot s = await uploadTask.onComplete;
-      String downloadUrl = await s.ref.getDownloadURL();
-      unsavedData["images"].add(downloadUrl);
-    }
-    setState(() {
-      imagesToUpload.clear();
-    });
-  }
+  _ProductScreenState(DocumentSnapshot product, String categoryId) :
+        _productModel = ProductModel(categoryId: categoryId, product: product);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      backgroundColor: Colors.grey[850],
-      appBar: AppBar(
-        elevation: 0,
-        title: Text(widget.product != null ? "Editar Produto" : "Criar Produto"),
-        actions: <Widget>[
-          product != null ?
-            IconButton(
-              icon: Icon(Icons.remove),
-              onPressed: (){
-                product.reference.delete();
-                Navigator.of(context).pop();
-              },
-            ):
-              Container(),
-          IconButton(
-            icon: Icon(Icons.save),
-            color: Colors.white,
-            onPressed: () async {
-              if(_formKey.currentState.validate()
-                  && (unsavedData["images"].length != 0 || imagesToUpload.length != 0)
-                  && unsavedData["sizes"].length != 0){
-                _formKey.currentState.save();
-
-                ScaffoldFeatureController s = _scaffoldKey.currentState.showSnackBar(
-                    SnackBar(
-                      content: Text("Salvando produto...", style: TextStyle(color: Colors.white),),
-                      duration: Duration(minutes: 1),
-                      backgroundColor: Colors.pinkAccent,
-                    )
-                );
-
-                try {
-                  if(product != null) {
-                    await uploadImages(product.documentID);
-                    await product.reference.updateData(unsavedData);
-                  } else {
-                    DocumentReference d = await Firestore.instance.collection("products").document(
-                        categoryId)
-                        .collection("items").add(unsavedData);
-                    await uploadImages(d.documentID);
-                    await d.updateData(unsavedData);
-                  }
-                  s.close();
-                  _scaffoldKey.currentState.showSnackBar(
-                      SnackBar(content: Text("Produto salvo!", style: TextStyle(color: Colors.white),),
-                        backgroundColor: Colors.pinkAccent,
-                      )
-                  );
-                } catch (e){
-
-                }
-              }
+    return ScopedModel<ProductModel>(
+      model: _productModel,
+      child: Scaffold(
+        key: _scaffoldKey,
+        backgroundColor: Colors.grey[850],
+        appBar: AppBar(
+          elevation: 0,
+          title: ScopedModelDescendant<ProductModel>(
+            builder: (context, child, model){
+              return Text(model.createdProduct ? "Editar Produto" : "Criar Produto");
             },
           ),
-        ],
-      ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.all(16),
-          children: <Widget>[
-            Text(
-              "Imagens",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-              ),
-            ),
-            Container(
-              height: 124,
-              padding: EdgeInsets.only(top: 16, bottom: 8),
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: unsavedData["images"].map<Widget>((i){
-                  return Container(
-                    height: 100,
-                    width: 100,
-                    margin: EdgeInsets.only(right: 8),
-                    child: GestureDetector(
-                      child: Image.network(i, fit: BoxFit.cover,),
-                      onLongPress: (){
-                        setState(() {
-                          unsavedData["images"].remove(i);
-                        });
-                      },
-                    )
-                  );
-                }).toList()..addAll(
-                  imagesToUpload.map<Widget>((i){
-                    return Container(
-                      height: 100,
-                      width: 100,
-                      margin: EdgeInsets.only(right: 8),
-                      child: GestureDetector(
-                        child: Image.file(i, fit: BoxFit.cover,),
-                        onLongPress: (){
-                          setState(() {
-                            imagesToUpload.remove(i);
-                          });
-                        },
-                      )
-                    );
-                  }).toList()
-                )..add(
-                  GestureDetector(
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      child: Icon(Icons.camera_enhance, color: Colors.white,),
-                      color: Colors.white.withAlpha(50),
-                    ),
-                    onTap: (){
-                      showModalBottomSheet(
-                        context: context,
-                        builder: (context) => ImageSourceSheet((image){
-                          Navigator.of(context).pop();
-                          setState(() {
-                            imagesToUpload.add(image);
-                          });
-                        })
-                      );
-                    },
-                  )
-                ),
-              ),
-            ),
-            SizedBox(height: 16,),
-            TextFormField(
-              style: TextStyle(color: Colors.white,
-                  fontSize: 16),
-              decoration: InputDecoration(
-                  labelText: "Título",
-                  labelStyle: TextStyle(color: Colors.grey),
-              ),
-              initialValue: unsavedData["title"],
-              onSaved: (text){unsavedData["title"] = text;},
-              validator: (text){
-                if(text.trim().isEmpty) return "Preencha o título do produto";
+          actions: <Widget>[
+            ScopedModelDescendant<ProductModel>(
+              builder: (context, child, model){
+                return model.createdProduct ?
+                IconButton(
+                  icon: Icon(Icons.remove),
+                  onPressed: model.loading ? null : (){
+                    model.deleteProduct();
+                    Navigator.of(context).pop();
+                  },
+                ):
+                Container();
               },
             ),
-            TextFormField(
-              style: TextStyle(color: Colors.white,
-                  fontSize: 16),
-              maxLines: 6,
-              decoration: InputDecoration(
-                  labelText: "Descrição",
-                  labelStyle: TextStyle(color: Colors.grey)
-              ),
-              initialValue: unsavedData["description"],
-              onSaved: (text){unsavedData["description"] = text;},
-              validator: (text){
-                if(text.trim().isEmpty) return "Preencha a descrição do produto";
+            ScopedModelDescendant<ProductModel>(
+              builder: (context, child, model){
+                return IconButton(
+                    icon: Icon(Icons.save),
+                    color: Colors.white,
+                    onPressed: model.loading ? null : saveProduct
+                );
               },
-            ),
-            TextFormField(
-              style: TextStyle(color: Colors.white,
-                  fontSize: 16),
-              decoration: InputDecoration(
-                  labelText: "Preço",
-                  labelStyle: TextStyle(color: Colors.grey),
-              ),
-              keyboardType: TextInputType.number,
-              initialValue: unsavedData["price"]?.toStringAsFixed(2),
-              onSaved: (text){unsavedData["price"] = double.parse(text);},
-              validator: (text){
-                try{
-                  double price = double.parse(text);
-                  if(price.toString().split(".")[1].length != 2)
-                    return "Utilize 2 casas decimais";
-                } catch (e) {
-                  return "Preço inválido";
-                }
-              },
-            ),
-            SizedBox(height: 16,),
-            SizedBox(
-              height: 34.0,
-              child: GridView(
-                padding: EdgeInsets.symmetric(vertical: 4.0),
-                scrollDirection: Axis.horizontal,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 1,
-                    mainAxisSpacing: 8.0,
-                    childAspectRatio: 0.5
-                ),
-                children: ["PP", "P", "M", "G", "GG", "XG", "XXG"].map(
-                    (s){
-                  return GestureDetector(
-                    onTap: (){
-                      setState(() {
-                        if(unsavedData["sizes"].contains(s))
-                          unsavedData["sizes"].remove(s);
-                        else
-                          unsavedData["sizes"].add(s);
-                      });
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                        border: Border.all(
-                          color: unsavedData["sizes"].contains(s) ? Colors.pinkAccent : Colors.grey[500],
-                          width: 3.0
-                        )
-                      ),
-                      width: 50.0,
-                      alignment: Alignment.center,
-                      child: Text(s,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  );
-                    }
-                ).toList(),
-              ),
-            ),
+            )
           ],
+        ),
+        body: Stack(
+          children: [
+            Form(
+            key: _formKey,
+            child: ListView(
+              shrinkWrap: true,
+              padding: EdgeInsets.all(16),
+              children: <Widget>[
+                Text(
+                  "Imagens",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12,
+                  ),
+                ),
+                ImagesWidget(_productModel.unsavedData["images"]),
+                SizedBox(height: 16,),
+                TextFormField(
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                  decoration: InputDecoration(
+                    labelText: "Título",
+                    labelStyle: TextStyle(color: Colors.grey),
+                  ),
+                  initialValue: _productModel.unsavedData["title"],
+                  onSaved: (text){_productModel.unsavedData["title"] = text;},
+                  validator: (text){
+                    if(text.trim().isEmpty) return "Preencha o título do produto";
+                  },
+                ),
+                TextFormField(
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16),
+                  maxLines: 6,
+                  decoration: InputDecoration(
+                      labelText: "Descrição",
+                      labelStyle: TextStyle(color: Colors.grey)
+                  ),
+                  initialValue: _productModel.unsavedData["description"],
+                  onSaved: (text){_productModel.unsavedData["description"] = text;},
+                  validator: (text){
+                    if(text.trim().isEmpty) return "Preencha a descrição do produto";
+                  },
+                ),
+                TextFormField(
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16),
+                  decoration: InputDecoration(
+                    labelText: "Preço",
+                    labelStyle: TextStyle(color: Colors.grey),
+                  ),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  initialValue: _productModel.unsavedData["price"]?.toStringAsFixed(2),
+                  onSaved: (text){_productModel.unsavedData["price"] = double.parse(text);},
+                  validator: (text){
+                    double price = double.tryParse(text);
+                    if(price != null){
+                      if(!text.contains(".") || text.split(".")[1].length != 2)
+                        return "Utilize 2 casas decimais";
+                    } else {
+                      return "Preço inválido";
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+          ScopedModelDescendant<ProductModel>(
+            builder: (context, child, model) {
+              return IgnorePointer(
+                ignoring: !model.loading,
+                child: Container(
+                  color: model.loading ? Colors.black54 : Colors.transparent,
+                ),
+              );
+            },
+          )
+        ]
         ),
       ),
     );
+  }
+
+  void saveProduct () async {
+    if(_formKey.currentState.validate()){
+      _formKey.currentState.save();
+
+      ScaffoldFeatureController s = _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text("Salvando produto...", style: TextStyle(color: Colors.white),),
+          duration: Duration(minutes: 1),
+          backgroundColor: Colors.pinkAccent,
+        )
+      );
+
+      bool success = await _productModel.saveProduct();
+
+      s.close();
+
+      _scaffoldKey.currentState.showSnackBar(
+          SnackBar(
+            content: Text(success ? "Produto salvo!" : "Erro ao salvar produto!", style: TextStyle(color: Colors.white),),
+            duration: Duration(minutes: 1),
+            backgroundColor: Colors.pinkAccent,
+          )
+      );
+    }
   }
 }
---
+++ lib/widgets/images_widget.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:store_owner/widgets/image_source_sheet.dart';
+
+class ImagesWidget extends StatefulWidget {
+
+  final List images;
+
+  ImagesWidget(this.images);
+
+  @override
+  _ImagesWidgetState createState() => _ImagesWidgetState();
+}
+
+class _ImagesWidgetState extends State<ImagesWidget> {
+
+  @override
+  Widget build(BuildContext context) {
+    return FormField(
+      validator: (value){
+        if(widget.images.isEmpty)
+          return "Adicione imagens";
+      },
+      builder: (formFieldState) => Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            height: 124,
+            padding: EdgeInsets.only(top: 16, bottom: 8),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: widget.images.map<Widget>((i){
+                return Container(
+                    height: 100,
+                    width: 100,
+                    margin: EdgeInsets.only(right: 8),
+                    child: GestureDetector(
+                      child: i is String ? Image.network(i, fit: BoxFit.cover,) : Image.file(i, fit: BoxFit.cover,),
+                      onLongPress: (){
+                        setState(() {
+                          widget.images.remove(i);
+                        });
+                      },
+                    )
+                );
+              }).toList()..add(
+                  GestureDetector(
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      child: Icon(Icons.camera_enhance, color: Colors.white,),
+                      color: Colors.white.withAlpha(50),
+                    ),
+                    onTap: (){
+                      showModalBottomSheet(
+                          context: context,
+                          builder: (context) => ImageSourceSheet((image){
+                            Navigator.of(context).pop();
+                            setState(() {
+                              widget.images.add(image);
+                            });
+                          })
+                      );
+                    },
+                  )
+              ),
+            ),
+          ),
+          formFieldState.hasError ? Text(
+              formFieldState.errorText,
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 12
+            ),
+          ) : Container()
+        ],
+      )
+    );
+  }
+}
