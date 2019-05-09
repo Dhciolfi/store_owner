0f2157324fe2891d8ed7020d6620526c257c30d5
dhciolfi <daniel.ciolfi@gmail.com>
2019-05-08 23:04:34 -0300

    Added iOS support

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:store_owner/screens/login_screen.dart';
 
@@ -7,6 +8,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+    FirebaseAuth.instance.signOut();
+
     return MaterialApp(
       title: 'Store Owner',
       home: LoginScreen(),
